cc748a0bada4f93e2269d565c987714dd77dc1e1
nyramana <nyramana@student.42antananarivo.mg>
2026-07-22 14:48:54 +0300

    refactor: Removing the function from the original file

--- coders/destroy_all.c
+++ coders/destroy_all.c
@@ -6,7 +6,7 @@
 /*   By: nyramana <nyramana@student.42antananarivo  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/07 17:30:13 by nyramana          #+#    #+#             */
-/*   Updated: 2026/07/07 17:34:15 by nyramana         ###   ########.fr       */
+/*   Updated: 2026/07/22 14:48:29 by nyramana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,28 +22,6 @@ void	destroy_all(t_all *all)
 	destroy_coders(all->coders);
 }
 
-static void	destroy_dongle(t_dongle *dongle)
-{
-	pthread_mutex_destroy(&dongle->mutex);
-	pthread_mutex_destroy(&dongle->fifo.mutex);
-	free(dongle->fifo.array);
-	pthread_mutex_destroy(&dongle->edf.mutex);
-	free(dongle->edf.array);
-}
-
-int	destroy_dongles(t_all *all)
-{
-	int	i;
-
-	i = 0;
-	while (i < all->params.nb_coders)
-	{
-		destroy_dongle(&all->dongles[i]);
-		i++;
-	}
-	free(all->dongles);
-	return (1);
-}
 
 static int	destroy_coders(t_coder *coders)
 {
